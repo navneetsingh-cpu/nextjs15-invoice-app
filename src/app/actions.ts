6f13bdb5d71f88817243bc2c828e7668f258d0ca
Navneet <hunnyhunnyhunny@gmail.com>
2025-01-05 09:09:07 -0500

    Added Update Invoice status db call

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,9 +1,11 @@
 "use server";
 
-import { Invoices } from "@/db/schema";
+import { Invoices, Status } from "@/db/schema";
 import { db } from "@/db";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 
 export async function createAction(formData: FormData) {
@@ -22,4 +24,21 @@ export async function createAction(formData: FormData) {
     }).returning({ id: Invoices.id });
 
     redirect(`/invoices/${results[0].id}`);
+}
+
+export async function updateStatusAction(formData: FormData) {
+    const { userId } = await auth();
+    if (!userId) {
+        return; // Disable for unauthroized users
+    }
+
+    const id = parseInt(formData.get('id') as string);
+    const status = formData.get('status') as Status;
+
+    await db.update(Invoices)
+        .set({ status })
+        .where(and(eq(Invoices.id, id), eq(Invoices.userId, userId)))
+
+    revalidatePath(`/invoices/${id}`);
+
 }
--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -5,6 +5,18 @@ import { cn } from "@/lib/utils";
 import { eq, and } from "drizzle-orm";
 import { notFound } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
+import Container from "@/components/Container";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { AVAILABLE_STATUSES } from "@/data/invoices";
+import { updateStatusAction } from "@/app/actions";
+import { ChevronDown } from "lucide-react";
 
 export default async function InvoicePage({
   params,
@@ -35,60 +47,83 @@ export default async function InvoicePage({
   }
 
   return (
-    <main className="h-screen max-w-5xl mx-auto my-12">
-      <div className="flex justify-between mb-8">
-        <h1 className="flex items-center gap-4 text-3xl font-semibold">
-          Invoice {invoiceIdKey}
-          <Badge
-            className={cn(
-              "rounded-full capitalize",
-              result.status === "open" && "bg-blue-500",
-              result.status === "paid" && "bg-green-600",
-              result.status === "void" && "bg-zinc-700",
-              result.status === "uncollectable" && "bg-red-600"
-            )}
-          >
-            {result.status}
-          </Badge>
-        </h1>
-        <p></p>
-      </div>
+    <main className="w-full h-screen">
+      <Container>
+        <div className="flex justify-between mb-8">
+          <h1 className="flex items-center gap-4 text-3xl font-semibold">
+            Invoice {invoiceIdKey}
+            <Badge
+              className={cn(
+                "rounded-full capitalize",
+                result.status === "open" && "bg-blue-500",
+                result.status === "paid" && "bg-green-600",
+                result.status === "void" && "bg-zinc-700",
+                result.status === "uncollectable" && "bg-red-600"
+              )}
+            >
+              {result.status}
+            </Badge>
+          </h1>
 
-      <p className="text-3xl mb-3">${(result.value / 100).toFixed(2)}</p>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="flex items-center gap-2">
+                Change Status <ChevronDown className="w-4 h-auto" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {AVAILABLE_STATUSES.map((status) => (
+                <DropdownMenuItem key={status.id}>
+                  <form action={updateStatusAction}>
+                    <input type="hidden" name="id" value={invoiceIdKey} />
+                    <input type="hidden" name="status" value={status.id} />
 
-      <p className="text-lg mb-8">{result.description}</p>
+                    <button>{status.label}</button>
+                  </form>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
 
-      <h2 className="font-bold text-lg mb-4">Billing Details</h2>
+          <p></p>
+        </div>
 
-      <ul className="grid gap-2">
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Invoice ID
-          </strong>
-          <span>{invoiceIdKey}</span>
-        </li>
+        <p className="text-3xl mb-3">${(result.value / 100).toFixed(2)}</p>
 
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Invoice Date
-          </strong>
-          <span> {new Date(result.createTs).toLocaleDateString()}</span>
-        </li>
+        <p className="text-lg mb-8">{result.description}</p>
 
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Billing Name
-          </strong>
-          <span></span>
-        </li>
+        <h2 className="font-bold text-lg mb-4">Billing Details</h2>
 
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Billing Email
-          </strong>
-          <span></span>
-        </li>
-      </ul>
+        <ul className="grid gap-2">
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice ID
+            </strong>
+            <span>{invoiceIdKey}</span>
+          </li>
+
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Date
+            </strong>
+            <span> {new Date(result.createTs).toLocaleDateString()}</span>
+          </li>
+
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Name
+            </strong>
+            <span></span>
+          </li>
+
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Email
+            </strong>
+            <span></span>
+          </li>
+        </ul>
+      </Container>
     </main>
   );
 }
